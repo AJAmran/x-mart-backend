7286bf928b26bc1e731347d626e1762e0312d48a
Md Amran Hossen <mdamranhossen77@gmail.com>
2025-02-09 23:00:26 +0600

    added response sender

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,15 @@
+import config from "../config";
+import { AuthService } from "../services/authService";
 import { catchAsync } from "../utils/catchAsync";
 
-const registeruser = catchAsync(async (req, res)=>{
-    const result = await Auth
-})
+const registeruser = catchAsync(async (req, res) => {
+  const result = await AuthService.registerUser(req.body);
+  const { accessToken, refreshToken } = result;
+
+  res.cookie("refreshToken", refreshToken, {
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  
+});
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,13 +1,50 @@
+import { USER_ROLE } from "../constants/userConstant";
 import { TRegisterUser } from "../interface/authInterface";
 import { User } from "../models/User";
+import AppError from "../utils/appError";
+import httpStatus from "http-status";
+import { createToken } from "../utils/VerifyJWt";
+import config from "../config";
 
+const registerUser = async (payload: TRegisterUser) => {
+  // checking if the user is exist
+  const user = await User.isUserExistsByEmail(payload?.email);
 
+  if (user) {
+    throw new AppError(httpStatus.NOT_FOUND, "This user is already exist!");
+  }
 
-const registerUser = async(payload: TRegisterUser) => {
-    // check if the user is exist
-    const user = await User.isUserExistsByEmail(payload.email);
+  payload.role = USER_ROLE.USER;
 
-    if(user){
-        throw new Error("User already exists");
-    }
-}
+  //create new user
+  const newUser = await User.create(payload);
+
+  //create token and sent to the  client
+
+  const jwtPayload = {
+    _id: newUser._id,
+    name: newUser.name,
+    email: newUser.email,
+    mobileNumber: newUser.mobileNumber,
+    role: newUser.role,
+    status: newUser.status,
+  };
+
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwtSecret as string,
+    config.jwtExpiresIn as string
+  );
+
+  const refreshToken = createToken(
+    jwtPayload,
+    config.refreshSecret as string,
+    config.refreshExpiresIn as string
+  );
+
+  return { accessToken, refreshToken };
+};
+
+export const AuthService = {
+  registerUser,
+};
