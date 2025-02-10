0a215fb777b372a683d4b3339c474c5c5990d7c9
Md Amran Hossen <mdamranhossen77@gmail.com>
2025-02-10 23:50:52 +0600

    not found page added

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import AutRoute from "./routes/authRoutes";
 import cookieParser from "cookie-parser";
 import httpStatus from "http-status";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -14,15 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", AutRoute);
 
-
 //Testing
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.OK).json({
     success: true,
-    message: 'Welcome to the Lost And Found API',
+    message: "Welcome to the Lost And Found API",
   });
 });
 
+//global error handler
+app.use(globalErrorHandler);
+
+
+//handle not found
 
 
 export default app;
